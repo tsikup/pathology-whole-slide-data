8aacf2bfde6c8891b79e3e28f39bef482c2ceaf9
Nikos Tsiknakis <tsiknakisn@gmail.com>
2022-10-02 20:53:07 +0200

    update qupath annotation parser

--- wholeslidedata/annotation/parser.py
+++ wholeslidedata/annotation/parser.py
@@ -189,24 +189,31 @@ class WholeSlideAnnotationParser(AnnotationParser):
 class QuPathAnnotationParser(AnnotationParser):
     @staticmethod
     def get_available_labels(opened_annotation: dict):
-        return Labels.create(
-            set(
-                [
-                    # Label is rgbint (see https://stackoverflow.com/questions/2262100/rgb-int-to-rgb-python)
-                    Label.create(
-                        annotation["properties"]["classification"]["name"], annotation["properties"]["classification"]["colorRGB"]
-                    )
-                    for annotation in opened_annotation
-                ]
-            )
-        )
+        # Label is rgbint (see https://stackoverflow.com/questions/2262100/rgb-int-to-rgb-python)
+        labels = []
+        for annotation in opened_annotation:
+            try:
+                _label = Label.create(
+                    annotation["properties"]["classification"]["name"],
+                    annotation["properties"]["classification"]["colorRGB"],
+                )
+                labels.append(_label)
+            except Exception:
+                pass
+
+        return Labels.create(set(labels))
 
     def _parse(self, path) -> List[dict]:
         with open(path) as json_file:
             data = json.load(json_file)
+        if type(data) is not list:
+            data = [data]
         labels = self._get_labels(data)
         for annotation in data:
-            label_name = annotation["properties"]["classification"]["name"].lower()
+            try:
+                label_name = annotation["properties"]["classification"]["name"].lower()
+            except:
+                label_name = None
             if label_name not in labels.names:
                 continue
             label = labels.get_label_by_name(label_name)
@@ -231,16 +238,35 @@ class QuPathAnnotationParser(AnnotationParser):
             return []
 
         annotations = []
-        for index, annotation in enumerate(self._parse(path)):
+        index = 0
+        for annotation in self._parse(path):
             annotation["index"] = index
             annotation["type"] = annotation["geometry"]["type"].lower()
-            # TODO: Implement multipolygon
-            assert annotation["type"] == "polygon", "Annotation type should be polygon. Multipolygon is not yet implemented."
-            annotation["coordinates"] = np.array(annotation["geometry"]["coordinates"][0])
-            annotation["label"] = self._rename_label(annotation["label"])
-            del annotation["properties"]
-            del annotation["geometry"]
-            annotations.append(Annotation.create(**annotation))
+            assert annotation["type"] in [
+                "polygon",
+                "multipolygon",
+            ], "Annotation type should be polygon or multipolygon."
+
+            if annotation["type"] == "polygon":
+                annotation["coordinates"] = np.array(
+                    annotation["geometry"]["coordinates"][0]
+                )
+                annotation["label"] = self._rename_label(annotation["label"])
+                del annotation["properties"]
+                del annotation["geometry"]
+                annotations.append(Annotation.create(**annotation))
+                index += 1
+
+            elif annotation["type"] == "multipolygon":
+                for coords in annotation["geometry"]["coordinates"]:
+                    new_annotation = dict(
+                        index=index,
+                        type="polygon",
+                        coordinates=coords[0],
+                        label=self._rename_label(annotation["label"]),
+                    )
+                    annotations.append(Annotation.create(**new_annotation))
+                    index += 1
 
         for hook in self._hooks:
             annotations = hook(annotations)
